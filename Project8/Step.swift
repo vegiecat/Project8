af08e55d23f532dd5a62fe973d3976b91aa5f44a
jleetokyo <justin.alias@me.com>
2014-12-01 23:21:15 -0800

    more setup

---
+++ Project8/Step.swift
@@ -0,0 +1,20 @@
+//
+//  Step.swift
+//  Project8
+//
+//  Created by Justin Lee on 12/1/14.
+//  Copyright (c) 2014 Vegiecat Studio. All rights reserved.
+//
+
+import UIKit
+
+class Step: NSObject {
+    
+    var stepImage : UIImage?
+    var stepText : String?
+    var stepOrder : Int?
+    
+    override init(){
+        super.init()
+    }
+}
---
+++ Project8/StepEditorController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Vegiecat Studio on 11/21/14.
+//  Copyright (c) 2014 Vegiecat Studio. All rights reserved.
+//
+
+
+import UIKit
+
+class StepEditorController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
